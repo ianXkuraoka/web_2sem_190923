35440437ecb372e52450dd6f002047ab062aae00
Ian <ian.xk9@gmail.com>
2023-09-19 11:46:08 -0300

    aula_190923

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,8 @@
+export const ListaProdutos =[
+
+    {id: 1, nome: 'Teclado Gamer', desc:'Mecanico',preco:200},
+    {id: 2, nome: 'Mouse Gamer', desc:'mouse',preco:300},
+    {id: 3, nome: 'Cadeira Gamer', desc:'top',preco:2200},
+    {id: 4, nome: 'phone Gamer', desc:'melhor som',preco:1500},
+    {id: 5, nome: 'mousepad Gamer', desc:'plastico',preco:100},
+]
